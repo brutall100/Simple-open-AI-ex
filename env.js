1eaf5a0e4182200f8fe58144e5b55a7f84bd6800
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-07-27 15:13:48 +0300

    V-0.0.3
    Added node modules

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: "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"
+        OPENAI_API_KEY: "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"
     }
 }
